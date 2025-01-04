4c7b8769311aa2ce139e9bbd6b113d9ef763f47b
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-01-05 00:19:11 +0600

    btn added in the populer menu sectiion

--- src/Pages/Home/PopulerMenu/PopulerMenu.jsx
+++ src/Pages/Home/PopulerMenu/PopulerMenu.jsx
@@ -26,6 +26,9 @@ const PopulerMenu = () => {
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
+      <button className="btn btn-outline border-0 border-b-4 flex">
+        View Full Menu
+      </button>
     </section>
   );
 };
